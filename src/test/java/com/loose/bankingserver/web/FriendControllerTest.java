33d6de34f19833e795cbaa1b9a68ea96928b720a
junho <wnsgh1357@gmail.com>
2023-03-02 23:03:52 +0900

    Swagger 적용

--- src/test/java/com/loose/bankingserver/web/FriendControllerTest.java
+++ src/test/java/com/loose/bankingserver/web/FriendControllerTest.java
@@ -2,21 +2,14 @@ package com.loose.bankingserver.web;
 
 import com.loose.bankingserver.service.FriendService;
 import com.loose.bankingserver.web.dto.FriendDto;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockHttpSession;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import javax.servlet.http.HttpSession;
 
 import java.util.ArrayList;
 import java.util.List;
